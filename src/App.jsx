5a30bcfbb4b27b0f5a300cf270d2ae6c30f212b6
Gian Lazaro <hello@gianlazaro.com>
2022-09-19 23:58:50 -0700

    add a splash image for style, add link styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import Form from './components/Form';
+import RegistrationSplash from './components/RegistrationSplash';
 import './styles/App.scss';
 
 function App() {
 
   return (
     <div className="App">
+      <RegistrationSplash/>
       <Form/>
     </div>
   )
---
+++ src/components/RegistrationSplash.jsx
@@ -0,0 +1,10 @@
+import '../styles/Splash.scss';
+
+function RegistrationSplash() {
+  return (
+    <div className="splash">
+    </div>
+  )
+}
+
+export default RegistrationSplash;
